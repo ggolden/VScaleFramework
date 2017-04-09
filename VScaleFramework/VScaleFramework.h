321afe94244aa2bcc65017aa5c19e2da411245b0
Glenn R. Golden <ggolden@etudes.org>
2017-04-09 13:52:33 -0400

    Initial Commit

---
+++ VScaleFramework/VScaleFramework.h
@@ -0,0 +1,19 @@
+//
+//  VScaleFramework.h
+//  VScaleFramework
+//
+//  Created by Glenn R. Golden on 4/9/17.
+//  Copyright © 2017 Glenn R. Golden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VScaleFramework.
+FOUNDATION_EXPORT double VScaleFrameworkVersionNumber;
+
+//! Project version string for VScaleFramework.
+FOUNDATION_EXPORT const unsigned char VScaleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VScaleFramework/PublicHeader.h>
+
+
